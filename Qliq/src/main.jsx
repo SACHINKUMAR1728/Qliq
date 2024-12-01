f4a724164b694bd6304bae8d3ff2ba13c59b5b4d
Sushil Pandey <contact.sushilpandey@gmail.com>
2024-12-01 12:54:37 +0530

    Added Strucutre with login

--- Qliq/src/main.jsx
+++ Qliq/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
 
+import App from './App';
+import Notfound from './pages/Notfound';
+import Login from './pages/login';
+import Homepage from './pages/Homepage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      {/* Public Routes */}
+      <Route index element={<Homepage />} />
+      <Route path="login" element={<Login />} /> {/* Corrected component name */}
+
+      <Route path="*" element={<Notfound />} />
+    </Route>
+  )
+);
+
+// Render the router provider
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <RouterProvider router={router} />
+);
